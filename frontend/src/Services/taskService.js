18437b50bbe7607f0cf8f73f4a67c181cd3fa002
aneena-joseph <aneena.j@polussolutions.com>
2025-01-15 17:30:02 +0530

    commit for api issue task

--- frontend/src/Services/taskService.js
+++ frontend/src/Services/taskService.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 export const getTasks = (selectedLimit) => {
-    return axios.get(`/api/getTask/${JSON.parse(localStorage.getItem('userData'))._id}/${selectedLimit}`);
+    return axios.get(`https://to-do-list-00q9.onrender.com/data/getTask/${JSON.parse(localStorage.getItem('userData'))._id}/${selectedLimit}`);
 };
 
 export const addTask = (taskData) => {
-    return axios.post('/api/createTask', taskData);
+    return axios.post('https://to-do-list-00q9.onrender.com/data/createTask', taskData);
 };
 
 export const updateTask = (taskId, taskData) => {
-    return axios.put(`/api/updatetask/${taskId}`, taskData);
+    return axios.put(`https://to-do-list-00q9.onrender.com/data/updatetask/${taskId}`, taskData);
 };
 
 export const updateStatus = (taskId, status) => {
-    return axios.patch(`/api/updateStatus/${taskId}`, {status});
+    return axios.patch(`https://to-do-list-00q9.onrender.com/data/updateStatus/${taskId}`, {status});
 };
 
 export const deleteTasks = (taskId) => {
-    return axios.delete(`/api/deleteTask/${taskId}`);
+    return axios.delete(`https://to-do-list-00q9.onrender.com/data/deleteTask/${taskId}`);
 };
