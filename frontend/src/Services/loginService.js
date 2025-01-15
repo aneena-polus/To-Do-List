f765c97708d9273f2459d3228c9d7ce2e744d0bb
aneena-joseph <aneena.j@polussolutions.com>
2025-01-15 17:14:47 +0530

    commit for api change

--- frontend/src/Services/loginService.js
+++ frontend/src/Services/loginService.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 export const getAllCountries = () => {
-    return axios.get("/api/countries");
+    return axios.get("https://to-do-list-00q9.onrender.com/data/countries");
 };
 
 export const getAllStates = (countryCode) => {
-    return axios.get(`/api/states/${countryCode}`);
+    return axios.get(`https://to-do-list-00q9.onrender.com/data/states/${countryCode}`);
 };
 
 export const onLogin = (userData) => {
-    return axios.post("/api/login",userData);
+    return axios.post("https://to-do-list-00q9.onrender.com/data/login",userData);
 };
 
 export const onSignUp = (userData) => {
-    return axios.post("/api/signup",userData);
+    return axios.post("https://to-do-list-00q9.onrender.com/data/signup",userData);
 };
 
