ebd2f9d0a1a7659391d1a6d2b7cccbfbeba1aabc
aneena-joseph <aneena.j@polussolutions.com>
2025-01-15 18:11:37 +0530

    commit revert

--- server/app.js
+++ server/app.js
@@ -9,12 +9,10 @@ import cors from 'cors';
 dotenv.config();
 const app = express();
 
-const corsOptions = {
-    origin: 'http://localhost:3000',  
-    credentials: true,               
-  };
-  
-  app.use(cors(corsOptions));
+app.use(cors({
+    credentials: true, 
+}));
+
 connectDb();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
